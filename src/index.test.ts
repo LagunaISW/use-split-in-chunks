a6bee363173cd32780fbd55db71d4084176c4f1d
Jhonatan Laguna <jlaguna@nearsoft.com>
2020-03-24 11:39:14 -0700

    Tests updated

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,29 +1,52 @@
-import { useMyHook } from './'
+import { useSplitInChunks } from "./";
 import { renderHook, act } from "@testing-library/react-hooks";
 
-// mock timer using jest
-jest.useFakeTimers();
+describe("useSplitInChunks", () => {
+  it("splits in chunks of 2", () => {
+    let names = ["Ted", "Tracy", "Marshal", "Lily", "Barney", "Robin"];
+    const { result } = renderHook(() => useSplitInChunks(names, 2));
 
-describe('useMyHook', () => {
-  it('updates every second', () => {
-    const { result } = renderHook(() => useMyHook());
+    expect(result.current.chunks.length).toBe(3);
 
-    expect(result.current).toBe(0);
-
-    // Fast-forward 1sec
     act(() => {
-      jest.advanceTimersByTime(1000);
+      names = ["Harry", "Hermione", "Ron"];
+      result.current.setArray(names);
     });
 
-    // Check after total 1 sec
-    expect(result.current).toBe(1);
+    expect(result.current.chunks.length).toBe(2);
+  });
 
-    // Fast-forward 1 more sec
+  it("splits in chunks of n", () => {
+    let names = ["Ted", "Tracy", "Marshal", "Lily", "Barney", "Robin"];
+    const { result } = renderHook(() => useSplitInChunks(names, 2));
+
+    expect(result.current.chunks.length).toBe(3);
+
     act(() => {
-      jest.advanceTimersByTime(1000);
+      result.current.setSize(3);
     });
 
-    // Check after total 2 sec
-    expect(result.current).toBe(2);
-  })
-})
+    expect(result.current.chunks.length).toBe(2);
+
+    act(() => {
+      result.current.setSize(1);
+    });
+
+    expect(result.current.chunks.length).toBe(6);
+  });
+
+  it("throws error on chunkSize 0", () => {
+    let names = ["Ted", "Tracy", "Marshal", "Lily", "Barney", "Robin"];
+    const { result } = renderHook(() => useSplitInChunks(names, 0));
+
+    expect(result.error).toEqual(Error("chunkSize should be greater than 0"));
+  });
+
+  it("throws error on not an array", () => {
+    const { result } = renderHook(() =>
+      useSplitInChunks((undefined as unknown) as [], 2)
+    );
+
+    expect(result.error).toEqual(Error("first argument should be an array"));
+  });
+});
