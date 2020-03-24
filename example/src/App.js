1598131651d7955a201b818b9627de7c2ba016a5
Jhonatan Laguna <jlaguna@nearsoft.com>
2020-03-24 11:39:34 -0700

    Example updated

--- example/src/App.js
+++ example/src/App.js
@@ -1,19 +1,48 @@
-import React from 'react'
+import React from "react";
 
-import { useSplitInChunks } from '@lagunaisw/use-split-in-chunks'
+import { useSplitInChunks } from "@lagunaisw/use-split-in-chunks";
 
 const App = () => {
-  const chunks = useSplitInChunks(["Ted", "Tracy", "Marshal", "Lily", "Barney", "Robin"], 1)
+  const names = [
+    "Ted",
+    "Tracy",
+    "Marshal",
+    "Lily",
+    "Barney",
+    "Robin",
+    "Harry",
+    "Hermione",
+    "Ron",
+    "Michael",
+    "Jim",
+    "Pam",
+    "Dwight"
+  ];
+  const { chunks, setSize } = useSplitInChunks(names, 2);
+
+  const handleMinus = () => {
+    setSize(prev => prev - 1);
+  };
+
+  const handlePlus = () => {
+    setSize(prev => prev + 1);
+  };
+
   return (
     <div>
+      <div>
+        <p>Change size:</p>
+        <button onClick={handleMinus}>-</button>
+        <button onClick={handlePlus}>+</button>
+      </div>
       {chunks.map((chunk, index) => (
-        <ul key={index}> 
+        <ul key={index}>
           {chunk.map((item, index) => (
-            <li key={index}>{item}</li>)
-          )}
-        </ul>)
-      )}
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      ))}
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
